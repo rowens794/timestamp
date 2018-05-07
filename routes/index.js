3d0130fb7ab63ec44d62b10d5c0513c6b09f78bd
Ryan Owens <rowens794@gmail.com>
2018-05-07 07:50:29 -0400

    completed timestamp app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
+
+var jsonTime = {
+  unix: 0,
+  natural: ""
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("directions");
+});
+
+/* GET home page. */
+router.get('/:timeString', function(req, res, next) {
+  dateString = req.params.timeString;
+
+  if (!moment(dateString).isValid()){
+    res.render("directions");
+    return;
+  }
+
+  jsonTime.unix = moment(dateString).unix();
+  jsonTime.natural = moment(dateString).format("MMMM Do YYYY");
+
+  res.send(jsonTime);
 });
 
 module.exports = router;
